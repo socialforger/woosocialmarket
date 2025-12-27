740975b5fc2f4983b57405190baf69955973315d
Socialforger <socialforger@gmail.com>
2025-12-27 15:42:18 +0100

    Update class-woosm-shortcodes.php

--- includes/class-woosm-shortcodes.php
+++ includes/class-woosm-shortcodes.php
@@ -1,1 +1,58 @@
+<?php
+/**
+ * Shortcodes frontend
+ * author: socialforger
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Woosm_Shortcodes {
+
+    public function __construct() {
+        add_shortcode( 'woosm_login', array( $this, 'render_login' ) );
+        add_shortcode( 'woosm_register', array( $this, 'render_register' ) );
+        add_shortcode( 'woosm_reserved_home', array( $this, 'render_reserved_home' ) );
+    }
+
+    public function render_login( $atts = array(), $content = '' ) {
+        if ( is_user_logged_in() ) {
+            wp_safe_redirect( woosm_get_reserved_home_url() );
+            exit;
+        }
+
+        ob_start();
+        include WOOSM_PLUGIN_DIR . 'public/templates/login-form.php';
+        return ob_get_clean();
+    }
+
+    public function render_register( $atts = array(), $content = '' ) {
+        if ( is_user_logged_in() ) {
+            wp_safe_redirect( woosm_get_reserved_home_url() );
+            exit;
+        }
+
+        if ( empty( $_GET['email'] ) ) {
+            return '<p>' . esc_html__( 'Email non valida o mancante.', 'woosocialmarket' ) . '</p>';
+        }
+
+        $email       = sanitize_email( wp_unslash( $_GET['email'] ) );
+        $woosm_email = $email;
+
+        ob_start();
+        include WOOSM_PLUGIN_DIR . 'public/templates/registration-form.php';
+        return ob_get_clean();
+    }
+
+    public function render_reserved_home( $atts = array(), $content = '' ) {
+        if ( ! is_user_logged_in() ) {
+            wp_safe_redirect( woosm_get_login_page_url() );
+            exit;
+        }
+
+        ob_start();
+        include WOOSM_PLUGIN_DIR . 'public/templates/reserved-home.php';
+        return ob_get_clean();
+    }
+}
