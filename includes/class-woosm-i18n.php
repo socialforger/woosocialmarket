39020fe560cad5fe081dc8aadd0fddc4bd98afb4
Socialforger <socialforger@gmail.com>
2025-12-27 15:39:35 +0100

    Update class-woosm-i18n.php

--- includes/class-woosm-i18n.php
+++ includes/class-woosm-i18n.php
@@ -1,1 +1,20 @@
-TODO
+<?php
+/**
+ * Internazionalizzazione
+ * author: socialforger
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Woosm_i18n {
+
+    public function load_textdomain() {
+        load_plugin_textdomain(
+            'woosocialmarket',
+            false,
+            dirname( plugin_basename( __FILE__ ) ) . '/../languages/'
+        );
+    }
+}
