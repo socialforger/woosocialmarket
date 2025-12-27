ad57fad5d974d2f16ebb2aa897bf6c90955c865d
Socialforger <socialforger@gmail.com>
2025-12-27 15:41:42 +0100

    Create class-woosm-public.php

---
+++ includes/class-woosm-public.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Frontend public
+ * author: socialforger
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Woosm_Public {
+
+    public function enqueue_assets() {
+        wp_enqueue_style(
+            'woosm-public',
+            WOOSM_PLUGIN_URL . 'assets/css/public.css',
+            array(),
+            WOOSM_VERSION
+        );
+
+        wp_enqueue_script(
+            'woosm-public',
+            WOOSM_PLUGIN_URL . 'assets/js/public.js',
+            array( 'jquery' ),
+            WOOSM_VERSION,
+            true
+        );
+    }
+}
