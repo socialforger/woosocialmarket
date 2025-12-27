0d2f26a210b08f910ab3628d09a1bb4d9745e5ab
Socialforger <socialforger@gmail.com>
2025-12-27 15:38:33 +0100

    Update class-woosm-activator.php

--- includes/class-woosm-activator.php
+++ includes/class-woosm-activator.php
@@ -1,1 +1,17 @@
-TODO
+<?php
+/**
+ * Attivazione plugin
+ * author: socialforger
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Woosm_Activator {
+
+    public static function activate() {
+        // In futuro: registrazione CPT, ruoli, tabelle custom
+        flush_rewrite_rules();
+    }
+}
