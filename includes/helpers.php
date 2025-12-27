a62d76cf5de2cdad2fd8c74b52a9eb36b1c893cb
Socialforger <socialforger@gmail.com>
2025-12-27 15:40:02 +0100

    Update helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,1 +1,31 @@
+<?php
+/**
+ * Helper functions
+ * author: socialforger
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function woosm_log( $message ) {
+    if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+        if ( is_array( $message ) || is_object( $message ) ) {
+            $message = print_r( $message, true );
+        }
+        error_log( '[WooSocialMarket] ' . $message );
+    }
+}
+
+function woosm_get_login_page_url() {
+    // In futuro può diventare configurabile via impostazioni.
+    return site_url( '/woosm-login/' );
+}
+
+function woosm_get_registration_page_url() {
+    return site_url( '/woosm-registrazione/' );
+}
+
+function woosm_get_reserved_home_url() {
+    return site_url( '/area-riservata/' );
+}
